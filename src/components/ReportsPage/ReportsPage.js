280010e3e94730adbc9ce1c775e694569e905063
Tsaha11 <tuhin727066@gmail.com>
2024-04-13 05:34:17 +0530

    all features added

--- src/components/ReportsPage/ReportsPage.js
+++ src/components/ReportsPage/ReportsPage.js
@@ -6,53 +6,112 @@ import ReactECharts from 'echarts-for-react'
 import graphPoints from "../../store/data.json"
 import "./style.css"
 import { Audio } from 'react-loader-spinner';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { Tooltip } from 'antd';
+import XemplLogo from '../../assets/images/xempla-logo.svg'
+import NavIconMenu from '../../assets/images/nav-dropdown-icon.svg'
+import ThemeMoon from '../../assets/images/nightmode-nav-icon.svg'
+import LocationPin from '../../assets/images/location-nav-icon.svg'
+import WeatherIcon from '../../assets/images/weather-nav-icon.svg'
+import NotificationIcon from '../../assets/images/noti-nav-icon.svg'
+import UserNavIcon from '../../assets/images/user-nav-icon.svg'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const ReportsPage = () => {
     const [graphData,setgraphData]=useState([]);
     const [xaxis,setXaxis]=useState([])
     const [yaxis,setYaxis]=useState([])
+    const [avg,setAvg]=useState([]);
     const [names,setName]=useState([])
-    const [set,setSet]=useState(1);
     const [namesAbbr,setNamesAbbr]=useState([]);
+    const [pagination,setPagination]=useState(true);
+    const [count,setCount]=useState(1);
+    const [idx,setIdx]=useState([]);
+    const [p,setP]=useState(1);
+    const [mode,setMode]=useState(false);
+    const modeHanlder = () => {
+        if (mode === true) {
+            document.body.classList.add('dark-theme');
+            document.body.style.color = 'white';
+            setMode(false);
+        } else {
+            document.body.className=''
+            document.body.style.color = ''; // Reset to default color
+            setMode(true);
+        }
+    };
+    
     function findAbbr(str) {
         if (!str) return ""; 
         const words = str.split(" "); 
         const abbreviation = words.map(word => word.charAt(0)).join(""); 
         return abbreviation.toUpperCase();
     }
-    const fetchHanlder=async()=>{
+    const fetchHanlder=async(cnt)=>{
         try {
             const data=graphPoints
-            const size=15;
-            let mn=Math.min(size*set,data.length)
+            const size=10;
+            let mn=Math.min(size*cnt,data.length)
             const tempData=[]
-            let mx=Math.max(size*(set-1),0);
-            console.log(mx,mn)
+            let mx=Math.max(0,size*(cnt-1))
             for(let i=mx;i<mn;i++){
-                tempData.push({
-                    name:data[i].name,
-                    val1:data[i].value1,
-                    val2:data[i].value2,
-                    select:false
-                })
+                if(idx.includes(data[i].index)){
+                    tempData.push({
+                        index:data[i].index,
+                        name:data[i].name,
+                        val1:data[i].value1,
+                        val2:data[i].value2,
+                        select:true
+                    })
+                }
+                else{
+                    tempData.push({
+                        index:data[i].index,
+                        name:data[i].name,
+                        val1:data[i].value1,
+                        val2:data[i].value2,
+                        select:false
+                    })
+                }
             }
-            setSet(prev=>{
-                return prev+1;
-            })
-            setgraphData(prev=>[...prev,...tempData])   
-            
+            setgraphData(tempData)
         } catch (error) {
             console.error("Error fetching data:", error);
         }
-        setLoader(false)
     }
+    const addData = (data, ind) => {
+        if (!idx.includes(ind)) {
+            const updatedXaxis = [...xaxis, data.val1];
+            const updatedYaxis = [...yaxis, data.val2];
+            const updatedIdx = [...idx, ind];
+            const updateNames=[...namesAbbr,findAbbr(data.name)]
+            const updatedAvg=[...avg,(data.val1+data.val2)/2]
+            setXaxis(updatedXaxis);
+            setYaxis(updatedYaxis);
+            setAvg(updatedAvg)
+            setNamesAbbr(updateNames)
+            setIdx(updatedIdx);
+            fetchHanlder(p);
+        }
+    };
     useEffect(()=>{
-        fetchHanlder();
-        const container = containerRef.current;
-        container.addEventListener('scroll', handleScroll);
-        return () => {
-            container.removeEventListener('scroll', handleScroll);
-        };
+        fetchHanlder(1);
+        let cnt=(graphPoints.length+9)/10;
+        setCount(cnt)
     },[])
+    useEffect(()=>{
+        if(mode){
+            document.body.classList.add('dark-theme')
+            document.body.style={color:'white'}
+        }
+    },[])
+    const handlePageChange = async(event, page) => {
+        const cnt=await page
+        fetchHanlder(cnt);
+        setP(cnt);
+    };
     const option = {
         grid: {
             show: false,
@@ -182,7 +241,7 @@ const ReportsPage = () => {
                 tooltip: {
 
                 },
-                data: xaxis,
+                data: avg,
                 smooth: true,
                 itemStyle: {
                     color: '#0052cc',
@@ -195,15 +254,92 @@ const ReportsPage = () => {
         ]
     }   
     const containerRef = useRef(null);
-    const [loader,setLoader]=useState(false);
-    const handleScroll = () => {
-        const container = containerRef.current;
-        if (container.scrollTop + container.clientHeight >= container.scrollHeight && !loader) {
-            setLoader(true);
-            fetchHanlder();
+    
+    const changeTextHanlder=(env)=>{
+        const text=env.target.value.toLowerCase();
+        const regexPattern = new RegExp(text, 'gi');
+        const countOccurrences = (str) => {
+            const matches = str.match(regexPattern);
+            return matches ? matches.length : 0;
+        };
+        const data=graphPoints.filter(item=>
+            countOccurrences(item.name) > 0
+        )
+        let mn=data.length
+        const tempData=[]
+        let mx=0
+        console.log(idx)
+        for(let i=mx;i<mn;i++){
+            if(idx.includes(data[i].index)){
+                tempData.push({
+                    index:data[i].index,
+                    name:data[i].name,
+                    val1:data[i].value1,
+                    val2:data[i].value2,
+                    select:true
+                })
+            }
+            else{
+                tempData.push({
+                    index:data[i].index,
+                    name:data[i].name,
+                    val1:data[i].value1,
+                    val2:data[i].value2,
+                    select:false
+                })
+            }
         }
-    };
-    return (
+        setgraphData(tempData)
+    }
+    return (<>
+        <header className='w-100 product-header position-fixed'>
+        <div className="header-wrapper d-flex justify-content-between pe-3 position-relative">
+            <div className="left-header-content d-flex flex-row align-items-center">
+            <button className='xem-header-nav-btn d-flex justify-content-center align-items-center me-3' style={{ backgroundColor: mode ? 'var(#292929)' : 'var(--light-bg-color)' }}><img src={NavIconMenu} alt="nav" /></button>
+            <img src={XemplLogo} alt="Xempla" className='xempla-logo' />
+            </div>
+            <div className='right-header-content d-flex flex-row align-items-center'>
+            <Tooltip title="Change Theme">
+                <button className='squircle-btn theme-change-btn me-2' >
+                <img src={ThemeMoon} alt="change theme" onClick={modeHanlder}/>
+                </button>
+            </Tooltip>
+            <Tooltip title="Location: Mumbai, India">
+                <div className="weatherHeader d-flex flex-row align-items-center justify-content-center me-2">
+                <img src={LocationPin} alt="change theme" />
+                <p className="m-0 ps-2">Mumbai, India</p>
+                </div>
+            </Tooltip>
+            <Tooltip title="Weather">
+                <button className='squircle-btn wun-btn me-2'>
+                <img src={WeatherIcon} alt="weather" />
+                </button>
+            </Tooltip>
+            <Tooltip title="Notification">
+                <div className='position-relative me-2'>
+                <span className="notidot position-absolute"></span>
+                <button className='squircle-btn wun-btn me-0' onClick={()=>{
+                    toast("No notification",{
+                        position:'top-center'
+                    })
+                }}>
+                    <img src={NotificationIcon} alt="change theme" />
+                </button>
+                </div>
+            </Tooltip>
+
+            <div className='dropdown-usernav'>
+                <Tooltip title="Your Profile and settings">
+                <button className='squircle-btn wun-btn'>
+                    <img src={UserNavIcon} alt="change theme" />
+                </button>
+                </Tooltip>
+            </div>
+
+            </div>
+        </div>
+        </header>
+        <ToastContainer></ToastContainer>
         <div className="page-wrapper">
             <section className="create-Report-Form-Header ps-4 pe-4 pb-4" >
                 <div className='page-title'>
@@ -214,7 +350,7 @@ const ReportsPage = () => {
                         <div className='wrapper-in-con-opts p-4'>
                             <div className='input-search-wrapper position-relative'>
                                 <img src={SearchIcon} alt="image search" className='position-absolute search-icon' />
-                                <input type='text' className='input-seach w-100' placeholder='Search ...' />
+                                <input type='text' className='input-seach w-100' placeholder='Search ...'  onChange={changeTextHanlder}/>
                             </div>
                             {/* For No data Found */}
                             <div className='d-flex align-items-center flex-column pt-5 pb-5 no-data'>
@@ -226,22 +362,18 @@ const ReportsPage = () => {
                                 {
                                     graphData.map((data)=>(
                                         <>
-                                        <div className='indi-report-name d-flex mb-2' key={data.name}>
-                                            <span className='w-100'>{data.name}</span>
+                                        <div className={`${data.select && 'selected'} indi-report-name d-flex mb-2`} key={data.name} onClick={()=>{
+                                            addData(data,data.index)
+                                        }}>
+                                            <span className={`${data.select && 'selected-option'} w-100`}>{data.name}</span>
                                         </div>
                                         </>
                                     ))
                                 }
-                                {loader && <div className='loading'>
-                                <Audio
-                                    height="30"
-                                    width="60"
-                                    radius="9"
-                                    color="blue"
-                                    ariaLabel="loading"
-                                    wrapperStyle
-                                    wrapperClass
-                                />
+                                {pagination && <div className='pagination'>
+                                <Stack spacing={2}>
+                                    <Pagination onChange={handlePageChange} count={count} color='primary' />
+                                </Stack>
                                 </div>}
                             </div>
                         </div>
@@ -270,6 +402,7 @@ const ReportsPage = () => {
                 </div>
             </section>
         </div>
+        </>
     )
 }
 
