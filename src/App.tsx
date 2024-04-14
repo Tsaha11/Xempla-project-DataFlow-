127d5791d2a437d4f102ec21b17f034c91c6173c
Tsaha11 <tuhin727066@gmail.com>
2024-04-14 22:01:20 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.scss';
 import Reports from './pages/Reports';
-
 function App() {
   return (
     <Router>
--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReportsPage from '../../components/ReportsPage'
 import Header from '../../components/Header'
+import { Analytics } from "@vercel/analytics/react"
 
 const Reports = () => {
     return (
         <>
             <Header />
+            <Analytics/>
             <ReportsPage />
         </>
 
